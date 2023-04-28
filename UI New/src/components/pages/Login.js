cd17cd27509830450892390d4e3fa18c1a97bf40
sivapperujan <sivapperujan.20210577@iit.ac.lk>
2023-04-28 08:48:36 +0530

    My commit

---
+++ UI New/src/components/pages/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css"
+
+const Login = () => {
+  return <div className="primary-heading">Logged In</div>;
+};
+
+export default Login;
---
+++ UI New/src/components/pages/Sign.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css"
+
+const Sign = () => {
+  return <div className="primary-heading">Signed In</div>;
+};
+
+export default Sign;
